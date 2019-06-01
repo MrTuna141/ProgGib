5867d00e6b18b4bebfa93d41c8fd1447e9d7908f
MrTuna141 <mubbe141@gmail.com>
2019-06-01 13:38:06 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,59 @@
 let RectX = 30;
 let RectY = 30;
+let RectR = 30;
 
 let CirkX = 300;
 let CirkY = 300;
-
-
+let CirkR = 18;
 
 let CanvX = 400;
 let CanvY = 400;
+
+let bullet = [];
 
 function setup() {
     createCanvas(CanvX, CanvY);
 }
 
 function draw() {
+  
+  
+    if (keyIsDown(SHIFT)){
+      bullet.push(new Bullet(RectX,RectY))
+      console.log(bullet)
+    }
+  
+  
+  function Bullet(PX,PY){
+    this.speed = 1.5;
+    //PX = player position x
+    this.x = PX;
+    //PY = player position y
+    this.y = PY;
+    this.r = 5;
+    
+    this.show = function(){ 
+      circle(this.x,this.y,this.r);
+    }
+    
+    this.shoot = function() {
+      this.x += this.x+this.speed
+    }
+    
+    this.onScreen = function() {
+      return this.x > -this.r && this.x < width+this.r &&
+              this.y > -this.r && this.y < height+this.r;
+    }
+  }
+
+      let keepbullets = [];
+  let anyhit = false;
+  for (let i = 0; i < bullet.length;i++){
+    //FFS...............
+    bullet[i].shoot();
+    bullet[i].show();
+    
+  }
     function rectMove() {
         if (keyIsDown(87)) {
             RectY -= 5;
@@ -73,18 +113,7 @@ function draw() {
         CirkY -=5;
       }
     }
- //FIXA KULAN FFS 
-    let BulletRX = RectX+RectX/2;
-    let BulletRY = RectY+RectY/2;
   
-function PewPew() {
-  if (keyCode === SHIFT) {
-    circle(BulletRX,BulletRY,5);
-   // BulletRX += 5;
-    
-  } 
-}
-
     
     rectMove();
     cirkMove();
@@ -94,9 +123,8 @@ function PewPew() {
     stroke(255,0,255);
     line(200, 0, 200, 400);
     stroke(0);
-    //circle(BulletRX,BulletRY,5)
-    rect(RectX, RectY, 30, 30);
+    rect(RectX, RectY, 30, 30 );
     stroke(0);
-    circle(CirkX, CirkY, 18);
-    PewPew();
+    circle(CirkX, CirkY, CirkR);
+    
 }
